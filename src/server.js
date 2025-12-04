34136462a3a505a320be7b64dc689597cb0609b5
Marek Pavlíček <pavlicek.marecek1@seznam.cz>
2025-12-04 15:02:17 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json({ message: "He 🚀" });
+    res.json({ message: "Hello mmmm 🚀" });
 });
 
 if (process.env.NODE_ENV !== "test") {
